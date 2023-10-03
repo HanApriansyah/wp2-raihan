a6c215fe741dadb7711325d4c0895624a5dade80
hanapriansyah <raihanapriansyah2704@gmail.com>
2023-10-03 18:59:24 +0700

    Menambahkan Tugas Pertemuan 2 'contoh1, contoh2, contoh3'

---
+++ application/views/view_latihan.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>    
+
+<body>
+    Halo kawan.. yuk kita belajar web programming.!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
