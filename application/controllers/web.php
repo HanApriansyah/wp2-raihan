f5d526a32ed1efd3302acef4df03bb98d5f4f4a8
hanapriansyah <raihanapriansyah2704@gmail.com>
2023-10-14 13:19:57 +0700

    menambahkan method about()

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -7,9 +7,15 @@ class web extends CI_Controller{
         $this->load->helper('url');
     }
     public function index(){
-        $data['judul'] = "halaman depan";
+        $data['judul'] = "Halaman Depan";
         $this->load->view('v_header',$data);
         $this->load->view('v_index',$data);
+        $this->load->view('v_footer',$data);
+    }
+    public function about(){
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header',$data);
+        $this->load->view('v_about',$data);
         $this->load->view('v_footer',$data);
     }
 }
